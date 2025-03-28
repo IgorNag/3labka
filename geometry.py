6489588d3c049aa7f83e584285e2e162378d469a
Student <509-3@intc.local>
2025-03-28 09:58:42 +0500

    Рефакторинг кода

--- geometry.py
+++ geometry.py
@@ -1,9 +1,7 @@
 def calculate_area(length, width):
-    """Функция для вычисления площади прямоугольника"""
     if length <= 0 or width <= 0:
         raise ValueError("Длина и ширина должны быть положительными числами")
     return length * width
 
-#другой рпазработчик открывает файл и записывает изменения
 def calculate_perimeter(length, width):
     return 2 * (length + width)
