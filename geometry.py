bbe1cfe9ddcfaf2c60bb10f75e110c402d5a6b27
Student <509-3@intc.local>
2025-03-28 09:48:38 +0500

    initial commit

---
+++ geometry.py
@@ -0,0 +1,7 @@
+def calculate_area(length, width):
+    return length * width
+
+
+#другой рпазработчик открывает файл и записывает изменения
+def calculate_perimeter(length, width):
+    return 2 * (length + width)
